efc891e08d9fd93abc97895b0e2bb6def402dd88
Nick Waters <nickp60@users.noreply.github.com>
2024-04-22 13:02:16 -0400

    black format

--- fcid/test_fcid.py
+++ fcid/test_fcid.py
@@ -2,17 +2,21 @@ import subprocess
 
 from run import main, get_tech_type, FCIDs
 
+
 def test_by_machine(capsys):
     main(query="K12345", by_machine=True)
     outerr = capsys.readouterr()
-    assert outerr.out  == "HiSeq 3000,HiSeq 4000\n"
+    assert outerr.out == "HiSeq 3000,HiSeq 4000\n"
+
 
 def test_by_flowcell():
     print(get_tech_type("22C37GLT3", FCIDs))
     assert get_tech_type("22C37GLT3", FCIDs)[0][0] == "NovaSeq X"
 
+
 def test_by_flowcell_weird_zeros():
     assert get_tech_type("000000000-BLW8J", FCIDs)[0] == ["MiSeq"]
+
 
 def test_given_patterns():
     # here I have tried to list the pattern provided by illumina tech support
@@ -30,15 +34,15 @@ def test_given_patterns():
         "xxxxxAGxx": ["NextSeq 500", "/550 High Output"],
         "xxxxxBGxx": ["NextSeq 500", "/550 High Output"],
         "XXXXXXLT3": ["NovaSeq X"],
- 	"BNTxxxxx-xxxx":["iSeq 100"],
- 	"BPLxxxxx-xxxx":["iSeq 100"],
- 	"000Hxxxxx": ["MiniSeq"],
- 	"Dxxxx": ["MiSeq"],
- 	"Gxxxx": ["MiSeq"],
- 	"Lxxxx": ["MiSeq"],
+        "BNTxxxxx-xxxx": ["iSeq 100"],
+        "BPLxxxxx-xxxx": ["iSeq 100"],
+        "000Hxxxxx": ["MiniSeq"],
+        "Dxxxx": ["MiSeq"],
+        "Gxxxx": ["MiSeq"],
+        "Lxxxx": ["MiSeq"],
         "xxxxxAFxx": ["NextSeq 500"],
-        "xxxxxAGxx":["NextSeq 500"],
- 	"xxxxxBGxx": ["NextSeq 500"],
+        "xxxxxAGxx": ["NextSeq 500"],
+        "xxxxxBGxx": ["NextSeq 500"],
         "xxxxxxxM5": ["NextSeq 1000"],
         "xxxxxxxM5": ["NextSeq 1000"],
         "xxxxxxxHV": ["NextSeq 1000"],
@@ -51,15 +55,16 @@ def test_given_patterns():
         "xxxxxDRxx": ["NovaSeq 6000"],
         "xxxxxDMxx": ["NovaSeq 6000"],
         "xxxxxDSxx": ["NovaSeq 6000"],
-        "xxxxxxLTx": ["NovaSeq X"]
-        }
-    for k,v in templates.items():
+        "xxxxxxLTx": ["NovaSeq X"],
+    }
+    for k, v in templates.items():
         res = get_tech_type(k.upper(), FCIDs)
         print(res)
         # check machine
         assert res[0][0] == v[0]
         # check chemistry
         # TODO
+
 
 def test_by_novaseqx():
     assert get_tech_type("ABCDEFLT2", FCIDs)[0][0] == "NovaSeq X"
@@ -68,17 +73,29 @@ def test_by_novaseqx():
     assert get_tech_type("ABCDEFLT3", FCIDs)[1] == "10B flow cell"
     assert get_tech_type("ABCDEFLT4", FCIDs)[1] == "25B flow cell"
 
+
 def test_integration():
-    res = subprocess.check_output(["fcid ABCDEFLT3"], shell=True, )
+    res = subprocess.check_output(
+        ["fcid ABCDEFLT3"],
+        shell=True,
+    )
     print(res.decode())
-    assert res.decode()  == "NovaSeq X,NovaSeq X Plus\n"
+    assert res.decode() == "NovaSeq X,NovaSeq X Plus\n"
+
 
 def test_integration_detailed():
-    res = subprocess.check_output(["fcid ABCDEFLT3 --detailed"], shell=True, )
+    res = subprocess.check_output(
+        ["fcid ABCDEFLT3 --detailed"],
+        shell=True,
+    )
     print(res.decode())
-    assert res.decode()  == "ABCDEFLT3\tNovaSeq X,NovaSeq X Plus\t10B flow cell\n"
+    assert res.decode() == "ABCDEFLT3\tNovaSeq X,NovaSeq X Plus\t10B flow cell\n"
+
 
 def test_integration_bymachine():
-    res = subprocess.check_output(["fcid A12345 --by-machine"], shell=True, )
+    res = subprocess.check_output(
+        ["fcid A12345 --by-machine"],
+        shell=True,
+    )
     print(res.decode())
-    assert res.decode()  == "NovaSeq\n"
+    assert res.decode() == "NovaSeq\n"
